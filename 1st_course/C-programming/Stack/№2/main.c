116b90e3dc57a8c5cae1c74b8702a2aa94fbcba9
egortrue <estrukhin@gmail.com>
2020-04-02 14:23:18 +0300

    C-task #2 'Stack' was added

---
+++ 1st_course/C-programming/Stack/№2/main.c
@@ -0,0 +1,30 @@
+﻿#include "stack.h"
+#include <stdio.h>
+
+int main()
+{
+	// Read from a keyboard
+	char string[1000] = { 0 };
+	fgets(string, 1000, stdin);
+
+	// Push in the stack
+	int i = 0;
+	while (string[i]) StackPush(string[i++]);
+	StackPop(); // Remove last "Enter" symbol
+
+	i = 0;
+	while (!StackIsEmpty())
+	{
+		char symb = StackGet();
+		StackPop();
+		if (symb == string[i++]) continue;
+		else break;	
+	}
+
+	if (StackIsEmpty()) printf("This IS a polindrom");
+	else printf("This IS NOT a polindrom");
+
+	// Free the stack
+	StackDestroy();
+	return 0;
+}
