cf75b624da2e7c21ae691ef2e88505f1029bb2bc
egortrue <estrukhin@gmail.com>
2020-05-23 01:20:36 +0300

    C-task 'NanoChat' | init

---
+++ 1st_course/C-programming/NanoChat/src/client/client.c
@@ -0,0 +1,89 @@
+﻿
+#pragma comment(lib, "Ws2_32.lib")
+
+#include <winsock.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+void SendData2Server(int count, int number)
+{
+
+	// Create client
+	SOCKET client;
+	client = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (client == INVALID_SOCKET)
+	{
+		printf("Error create socket\n");
+		return;
+	}
+
+	// Connect to server
+	SOCKADDR_IN server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(5510); // server port
+	server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); // lookup | address of your own network adapter
+	if (connect(client, (SOCKADDR*)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Can't connect to server\n");
+		closesocket(client);
+		return;
+	}
+
+	// Send message
+	char message[1024];
+	sprintf_s(message, (size_t)1024, "<%d client> test %d", number, count);
+	int ret = send(client, message, strlen(message), 0);
+	if (ret == SOCKET_ERROR)
+	{
+		printf("Can't send message\n");
+		closesocket(client);
+		return;
+	}
+	printf("Send: %s\nbytes: %d\n\n", message, ret);
+
+	// Receive the answer from server
+	ret = SOCKET_ERROR;
+	while (ret == SOCKET_ERROR)
+	{
+		// Receive the text
+		ret = recv(client, message, 1024, 0);
+
+		if (ret == 0 || ret == WSAECONNRESET)
+		{
+			printf("Connection closed\n");
+			break;
+		}
+
+		if (ret < 0)
+		{
+			//printf("Can't receive message\n");
+			//closesocket(client);
+			//return;
+			continue;
+		}
+
+		// Output to local console
+		printf("Recieve: %s\nbytes: %d\n\n", message, ret);
+	}
+
+	closesocket(client);
+}
+
+int main()
+{
+	WSADATA wsd;
+	if (WSAStartup(MAKEWORD(1, 1), &wsd)) return 1;
+	printf("Connected to socket lib\n\n");
+
+	for (int i = 0; i < 10; i++)
+	{
+		SendData2Server(i, 1);
+		Sleep(1000);
+	}
+		
+	WSACleanup(); // It terminates use of the Winsock2 DLL (Ws2_32.dll).
+	return 0;
+}
+
---
+++ 1st_course/C-programming/NanoChat/src/server/server.c
@@ -0,0 +1,128 @@
+﻿
+#pragma comment(lib, "Ws2_32.lib")
+#define HAVE_STRUCT_TIMESPEC
+
+#include <pthread.h>
+#include <winsock.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+pthread_mutex_t mutex;
+
+void* ClientStart(void* param)
+{
+	SOCKET client = (SOCKET)param;
+	
+	// Receive the data from client
+	char recieve[1024];
+	int ret;
+	ret = recv(client, recieve, 1024, 0);
+	if (!ret || ret == SOCKET_ERROR)
+	{
+		pthread_mutex_lock(&mutex);
+		printf("Error getting data\n");
+		pthread_mutex_unlock(&mutex);
+		return (void*)1;
+	}
+	recieve[ret] = '\0';
+
+
+	// Print to server's console
+	pthread_mutex_lock(&mutex); 
+	printf("%s\n", recieve);
+	pthread_mutex_unlock(&mutex);
+
+
+	// Send the data to client
+	char transmit[1024];
+	sprintf_s(transmit, (size_t)1024, "Your data:\n%s\nwere received\0", recieve);
+	ret = send(client, transmit, strlen(transmit)+1, 0);
+	if (ret == SOCKET_ERROR)
+	{
+		pthread_mutex_lock(&mutex);
+		printf("Error sending data\n");
+		pthread_mutex_unlock(&mutex);
+		return (void*)2;
+	}
+
+
+	return (void*)0;
+}
+
+SOCKET ServerCreate()
+{
+	// Create a listening socket
+	SOCKET server = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);  // AF_INET - IPv4 address family
+	if (server == INVALID_SOCKET)                              // SOCK_STREAM - TCP protocol
+	{
+		printf("Error create server\n");
+		exit(1);
+	}
+
+	// Define the socket's address
+	SOCKADDR_IN server_addr;
+	server_addr.sin_family = AF_INET; 
+	server_addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(5510); // server port, must be more than 1024
+
+
+	// Bind the listening socket using the
+	// information in the sockaddr structure
+	if (bind(server, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	{
+		printf("Can't start server\n");
+		exit(2);
+	}
+
+	printf("Server is started\n");
+	return server;
+}
+
+
+void ServerRun(SOCKET server)
+{
+	SOCKET client;
+	SOCKADDR_IN client_addr;
+	int client_addr_size = sizeof(client_addr);
+
+	const time_t time_start = time(NULL); // Server startup time
+	listen(server, 50);                   // Create a waiting line for 50 clients
+	pthread_mutex_init(&mutex, NULL);     // Init access switcher for console 
+
+	printf("%s\n", ctime(&time_start));
+
+	// Listen for incoming connection requests on the server
+	while (1)
+	{	
+		// Get client data
+		client = accept(server, (SOCKADDR*)&client_addr, &client_addr_size);
+		if (client == INVALID_SOCKET)
+		{
+			printf("Error accept client\n");
+			continue;
+		}
+
+		// Create a thread for this client
+		pthread_t mythread;
+		pthread_create(&mythread, NULL, ClientStart, (void*)client);
+		pthread_detach(mythread);
+	}
+	
+	pthread_mutex_destroy(&mutex); // Destroy access switcher for console 
+	printf("Server is stopped\n");
+	closesocket(server);
+}
+
+int main()
+{
+	WSADATA wsd;
+	if (WSAStartup(MAKEWORD(1, 1), &wsd)) return 1;		
+	printf("Connected to socket lib\n");
+	
+	SOCKET server = ServerCreate();
+	ServerRun(server);
+
+	WSACleanup(); // It terminates use of the Winsock2 DLL (Ws2_32.dll).
+	return 0;
+}
+
