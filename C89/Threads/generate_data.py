a8613cd711b77f31b025745cffee45c6c8aa263c
egortrue <estrukhin@gmail.com>
2023-02-28 20:44:25 +0300

    C89 | Threads | update figures and code style

--- C89/Threads/generate_data.py
+++ C89/Threads/generate_data.py
@@ -1,22 +1,36 @@
 import random
 import argparse
 
+
 def handle_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('particles_count', action='store', type=int, help='Number of particles')
-    parser.add_argument('steps', action='store', type=int, help='Number of steps')
-    parser.add_argument('filename', action='store', type=str, help='Destination of generation')
+    parser.add_argument('particles_count', action='store',
+                        type=int, help='Number of particles')
+    parser.add_argument('steps', action='store',
+                        type=int, help='Number of steps')
+    parser.add_argument('filename', action='store', type=str,
+                        help='Destination of generation')
 
-    parser.add_argument('-mmin', type=float, help="Minimum mass for body (default=10000.0)", default=10000.0)
-    parser.add_argument('-mmax', type=float, help="Maximum mass for body (default=50000.0)", default=50000.0)
-    parser.add_argument('-xmin', type=float, help="Minimum x position for body (default=-10.0)", default=-10.0)
-    parser.add_argument('-xmax', type=float, help="Maximum x position for body (default=10.0)", default=10.0)
-    parser.add_argument('-ymin', type=float, help="Minimum y position for body (default=-10.0)", default=-10.0)
-    parser.add_argument('-ymax', type=float, help="Maximum y position for body (default=10.0)", default=10.0)
-    parser.add_argument('-vxmin', type=float, help="Minimum speed projection on x for body (default=-5.0)", default=-5.0)
-    parser.add_argument('-vxmax', type=float, help="Maximum speed projection on x for body (default=5.0)", default=5.0)
-    parser.add_argument('-vymin', type=float, help="Minimum speed projection on y for body (default=-5.0)", default=-5.0)
-    parser.add_argument('-vymax', type=float, help="Maximum speed projection on y for body (default=5.0)", default=5.0)
+    parser.add_argument(
+        '-mmin', type=float, help="Minimum mass for body (default=10000.0)", default=10000.0)
+    parser.add_argument(
+        '-mmax', type=float, help="Maximum mass for body (default=50000.0)", default=50000.0)
+    parser.add_argument(
+        '-xmin', type=float, help="Minimum x position for body (default=-10.0)", default=-10.0)
+    parser.add_argument(
+        '-xmax', type=float, help="Maximum x position for body (default=10.0)", default=10.0)
+    parser.add_argument(
+        '-ymin', type=float, help="Minimum y position for body (default=-10.0)", default=-10.0)
+    parser.add_argument(
+        '-ymax', type=float, help="Maximum y position for body (default=10.0)", default=10.0)
+    parser.add_argument(
+        '-vxmin', type=float, help="Minimum speed projection on x for body (default=-5.0)", default=-5.0)
+    parser.add_argument(
+        '-vxmax', type=float, help="Maximum speed projection on x for body (default=5.0)", default=5.0)
+    parser.add_argument(
+        '-vymin', type=float, help="Minimum speed projection on y for body (default=-5.0)", default=-5.0)
+    parser.add_argument(
+        '-vymax', type=float, help="Maximum speed projection on y for body (default=5.0)", default=5.0)
 
     return parser.parse_args()
 
